1dc2085b87944502f01ddee83a26cd4cb4320331
Fuumujin <42222948+Fuumujin@users.noreply.github.com>
2019-10-03 19:53:21 -0700

    Update Updating_webpage.sh

--- Updating_webpage.sh
+++ Updating_webpage.sh
@@ -1,3 +1,3 @@
 #1/bin/bash
-time=$(date)
-echo "<html><body><h1>Hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html
+time=$(date)                                                                                              #sets variable time
+echo "<html><body><h1>Hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html  #creates custom message
